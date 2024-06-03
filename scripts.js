1770ea2c10063f352e0520a701eb4e9f6e1b7eab
Ritik <shrestharitik@gmail.com>
2024-06-04 02:21:16 +0545

    change code for dark mode

--- scripts.js
+++ scripts.js
@@ -1,56 +1,8 @@
-// const checkbox = document.getElementById("navbar__checkbox");
-// const navbar = document.getElementsByClassName("navbar");
-// const navbarMenu = document.getElementsByClassName("navbar__menu");
-// const main = document.getElementsByClassName("main");
-// const mainDesc = document.getElementsByClassName("main-section__left");
-// const navbarImages =
-//   document.getElementsByClassName("navbar__images")[0].children;
-// const cardSection = document.getElementsByClassName("main__card-section");
-// const content = document.getElementsByClassName("exclusive-section__content");
-// const header = document.getElementsByTagName("h2");
-// console.log(mainDesc);
-// checkbox.addEventListener("change", () => {
-//   console.log(checkbox.checked);
-//   if (checkbox.checked) {
-//     document.body.classList.add("body--dark");
-//     navbar[0].classList.add("navbar--dark");
-//     main[0].classList.add("main--dark");
-//     for (let i = 0; i < mainDesc.length; i++)
-//       mainDesc[i].classList.add("text-color--dark");
-
-//     navbarMenu[0].classList.add("text-color--dark");
-//     for (let i = 0; i < cardSection.length; i++)
-//       cardSection[i].classList.add("text-color--dark");
-//     for (let i = 0; i < header.length; i++) {
-//       header[i].classList.add("text-color--dark");
-//     }
-//     for (let i = 0; i < content.length; i++) {
-//       content[i].classList.add("text-color--dark");
-//     }
-//     for (let i = 0; i < navbarImages.length; i++) {
-//       console.log(navbarImages[i]);
-//       navbarImages[i].classList.add("img-filter--dark");
-//     }
-//     mainDesc[0].getElementsByTagName("p")[0].classList.add("text-color--dark");
-//   } else {
-//     document.body.classList.remove("body--dark");
-
-//     navbar[0].classList.remove("navbar--dark");
-//     main[0].classList.remove("main--dark");
-//     mainDesc[0].classList.remove("text-color--dark");
-//     navbarMenu[0].classList.remove("text-color--dark");
-//     for (let i = 0; i < cardSection.length; i++)
-//       cardSection[i].classList.remove("text-color--dark");
-//     for (let i = 0; i < header.length; i++)
-//       header[i].classList.remove("text-color--dark");
-//     for (let i = 0; i < content.length; i++) {
-//       content[i].classList.add("text-color--dark");
-//     }
-//     mainDesc[0]
-//       .getElementsByTagName("p")[0]
-//       .classList.remove("text-color--dark");
-//     for (let i = 0; i < navbarImages.length; i++) {
-//       navbarImages[i].classList.remove("img-filter--dark");
-//     }
-//   }
-// });
+const checkbox = document.getElementById("navbar__checkbox");
+checkbox.addEventListener("change", () => {
+  if (checkbox.checked) {
+    document.body.classList.add("body--dark");
+  } else {
+    document.body.classList.remove("body--dark");
+  }
+});
